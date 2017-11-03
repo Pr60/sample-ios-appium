f34c74318c63b796336b7344c308a43cdf2348c8
Sasan Naderi <sasan@buddybuild.com>
2017-11-02 17:23:28 -0700

    Move dep setup to postbuild.

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,9 +1,45 @@
 #!/usr/bin/env bash
 
+chruby 2.4.1
+
+# # install brew, ant, maven
+# if ! which brew >/dev/null; then
+# 	echo "Installing brew..."
+# 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# fi
+
+# if ! which ant >/dev/null; then
+# 	echo "Installing ant..."
+# 	brew install ant
+# fi
+
+# if ! which maven >/dev/null; then
+# 	echo "Installing brew..."
+# 	brew install maven
+# fi
+
+# install appium
+npm install -g appium
+
+# authorize for testing
+npm install -g authorize-ios
+sudo authorize-ios
+
+# install rubygems
+bundle install 
+
 # start appium in background
 nohup appium &
+echo $! > $BUDDYBUILD_WORKSPACE/appium_pid.txt
 
 export APP_PATH=$BUDDYBUILD_PRODUCT_DIR
 
 cd $BUDDYBUILD_WORKSPACE
 bundle exec ruby simple_test.rb
+
+# terminate 
+jobs -l
+kill -9 `cat $BUDDYBUILD_WORKSPACE/appium_pid.txt`
+rm $BUDDYBUILD_WORKSPACE/appium_pid.txt
+
+exit 0
--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
-chruby 2.4.1
+# chruby 2.4.1
 
-# install brew, ant, maven
-if ! which brew >/dev/null; then
-	echo "Installing brew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+# # # install brew, ant, maven
+# # if ! which brew >/dev/null; then
+# # 	echo "Installing brew..."
+# # 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# # fi
 
-if ! which ant >/dev/null; then
-	echo "Installing ant..."
-	brew install ant
-fi
+# # if ! which ant >/dev/null; then
+# # 	echo "Installing ant..."
+# # 	brew install ant
+# # fi
 
-if ! which maven >/dev/null; then
-	echo "Installing brew..."
-	brew install maven
-fi
+# # if ! which maven >/dev/null; then
+# # 	echo "Installing brew..."
+# # 	brew install maven
+# # fi
 
-# install appium
-npm install -g appium
+# # install appium
+# npm install -g appium
 
-# authorize for testing
-npm install -g authorize-ios
-sudo authorize-ios
+# # authorize for testing
+# npm install -g authorize-ios
+# sudo authorize-ios
 
-# install rubygems
-bundle install
+# # install rubygems
+# bundle install 
