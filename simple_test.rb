f948e73115847c0da3cc91743a9898c17335a3fc
Sasan Naderi <sasan.naderi@gmail.com>
2017-11-24 17:14:22 -0800

    Update test.

--- simple_test.rb
+++ simple_test.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 require 'appium_lib'
+require 'rspec'
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+end
 
 # Retrieve the APP_PATH env var
 app_path =  `echo $APP_PATH`.chomp
@@ -20,21 +25,62 @@ desired_caps = {
   }
 }
 
-# Start the driver
-Appium::Driver.new(desired_caps, true).start_driver
+describe 'M2ViewController' do
+  before(:all) do
+    Appium::Driver.new(desired_caps).start_driver
+    Appium.promote_appium_methods RSpec::Core::ExampleGroup
+  end
 
-module TwentyFourtyEight
-  module IOS
-    # Add all the Appium library methods to Test to make
-    # calling them look nicer.
-    Appium.promote_singleton_appium_methods TwentyFourtyEight
+  def back_click(opts={})
+    opts        ||= {}
+    search_wait = opts.fetch(:wait, 10) # seconds
+    wait(search_wait) { button_exact('Back').click }
+  end
 
-    # Click the first button
-    button(1).click
-
-    # Quit when you're done!
+  after(:all) do
     driver_quit
-    puts 'Tests Succeeded!'
+  end
+
+  describe '.settings', :one do
+    subject { find_elements(:class_name, 'UIATableView')[0] }
+
+    it { should_not be nil }
+
+    context 'when used as a selection context' do
+      it 'Can be a selection context' do
+        rows = subject.find_elements(:class_name, 'UIATableCell')
+        rows.size.should eq 12
+      end
+
+      it 'does not return elements it does not contain' do
+        nav_bar = subject.find_elements(:class_name, 'UIANavigationBar')
+        nav_bar.length.should be 0
+      end
+    end
+
+    it 'returns its text' do
+      rows = subject.find_elements(:class_name, 'UIATableCell')
+      # rows.first.name.should eq 'Buttons, Various uses of UIButton'
+      rows.first.name.should eq 'Buttons'
+    end
   end
 end
+
+# # Start the driver
+# Appium::Driver.new(desired_caps, true).start_driver
+
+# module TwentyFourtyEight
+#   module IOS
+#     # Add all the Appium library methods to Test to make
+#     # calling them look nicer.
+#     Appium.promote_singleton_appium_methods TwentyFourtyEight
+
+#     # Click the first button
+#     button(1).click
+
+#     # Quit when you're done!
+#     driver_quit
+#     puts 'Tests Succeeded!'
+#   end
+# end
 
